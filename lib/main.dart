6d07a3186786e3fb6ba9dff424acc902e61b4801
Acer <Acer@LAPTOP-FTNPN94M>
2024-08-28 16:55:58 +0700

    latihan branch

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() {
     home: new Pertemuan2(),
   ));
 }
+
 //kkkkk
 class Pertemuan2 extends StatelessWidget {
   // This widget is the root of your application.
@@ -23,7 +24,6 @@ class Pertemuan2 extends StatelessWidget {
                 padding: new EdgeInsets.symmetric(vertical: 20.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
                   children: <Widget>[
                     new Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -40,7 +40,6 @@ class Pertemuan2 extends StatelessWidget {
                                 fontSize: 18.0)),
                       ],
                     ),
-
                     new Icon(
                       Icons.star,
                       size: 30.0,
@@ -96,12 +95,30 @@ class Pertemuan2 extends StatelessWidget {
                           ],
                         ),
                       ])),
-          new Padding(
-            padding: new EdgeInsets.symmetric(horizontal: 20.0),
-
-          )
-            ],
-          )),
+              new Padding(
+                  padding: new EdgeInsets.symmetric(vertical: 20.0),
+                  child: Container(
+                // Mengatur lebar teks agar tidak terlalu panjang
+                width: double.infinity,
+                child: Text(
+                  "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of "
+                      "classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin "
+                      "professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, "
+                      "consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical "
+                      "literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 "
+                      "of (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the "
+                      "theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, , comes from "
+                      "a line in section 1.10.32.",
+                  style: new TextStyle(
+                      color: Colors.black,
+                      fontFamily: "Serif",
+                      fontSize: 12.0),
+                  textAlign: TextAlign.justify, // Justify text untuk tampilan lebih rapi
+                ),
+              ),
+      )
+                      ]),
+      )
     );
   }
 }
